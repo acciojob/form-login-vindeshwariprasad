de9c5fe34f4bd938b989a3d091e8b41f763c8c4c
Vindeshwari Prasad <vinayguptasia53@gmail.com>
2024-03-26 18:44:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 function getFormvalue() {
     //Write your code here
+	var form = document.getElementById("form1");
+
+    // Get the values of first name and last name from the form
+    var firstName = form.fname.value;
+    var lastName = form.lname.value;
+
+    // Construct the full name
+    var fullName = firstName + " " + lastName;
+
+    // Display the full name using an alert
+    alert(fullName);
 
 }
